02d5e425a6022f97e2d5f67b84bba452a895ed19
Kyle Wu <89821773+kkyw30@users.noreply.github.com>
2022-07-26 15:14:04 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,12 +5,9 @@ import javax.swing.*;
 public class Game {
     /**
      * Main method run to start and run the game. Initializes the runnable game
-     * class of your choosing and runs it. IMPORTANT: Do NOT delete! You MUST
-     * include a main method in your final submission.
      */
     public static void main(String[] args) {
-        Runnable game = new org.cis120.Chess.RunChess(); // Set the game you want to run
-                                                         // here
+        Runnable game = new org.cis120.Chess.RunChess(); 
         SwingUtilities.invokeLater(game);
     }
 }
